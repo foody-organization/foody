d2e990f815847f12a870c41de880ea20f5da30ca
hyunsu <counman4@naver.com>
2025-06-12 14:01:02 +0900

    게시판 CRUD 화면 추가 boardController @RequestMapping("/board")에서 @RequestMapping("/api/board")으로 변경 엔티티 작성자(회원) 임의로 추가 회원테이블 구현시 수정해야됨

--- src/main/java/com/project/foody/board/dto/BoardDto.java
+++ src/main/java/com/project/foody/board/dto/BoardDto.java
@@ -13,6 +13,8 @@ public interface BoardDto {
     class Request {
         private String title;
         private String content;
+        //회원 회원테이블 컬럼명 바뀌면 수정예정
+        private String mem;
     }
 
     @NoArgsConstructor
@@ -22,5 +24,7 @@ public interface BoardDto {
     class Response extends BaseResponseDto {
         private String title;
         private String content;
+        //회원 회원테이블 컬럼명 바뀌면 수정예정
+        private String mem;
     }
 }
--- src/main/java/com/project/foody/board/service/BoardServiceImpl.java
+++ src/main/java/com/project/foody/board/service/BoardServiceImpl.java
@@ -27,6 +27,7 @@ public class BoardServiceImpl implements BoardService {
         Board board = Board.builder()
                 .title(boardDto.getTitle())
                 .content(boardDto.getContent())
+                .mem(boardDto.getMem())
                 .build();
 
         return boardRepository.save(board).getId();
@@ -49,6 +50,7 @@ public class BoardServiceImpl implements BoardService {
                 .id(board.getId())
                 .title(board.getTitle())
                 .content(board.getContent())
+                .mem(board.getMem())
                 .createDate(board.getCreateDate())
                 .updateDate(board.getUpdateDate())
                 .build();
@@ -69,6 +71,7 @@ public class BoardServiceImpl implements BoardService {
                         .id(board.getId())
                         .title(board.getTitle())
                         .content(board.getContent())
+                        .mem(board.getMem())
                         .createDate(board.getCreateDate())
                         .updateDate(board.getUpdateDate())
                         .build())
