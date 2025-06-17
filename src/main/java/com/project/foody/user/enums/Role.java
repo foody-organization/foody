607f34c023f55dcf11e25e4fdd7cb20f2dfdc0c4
naxomis <nayomi@hs.ac.kr>
2025-06-17 12:35:24 +0900

    회원엔티티구현
    회원엔티티구현했습니다

---
+++ src/main/java/com/project/foody/user/enums/Role.java
@@ -0,0 +1,7 @@
+package com.project.foody.user.enums;
+
+public enum Role {
+    USER,       // 일반 이용자 (음식점 검색, 리뷰, 예약)
+    OWNER,      // 식당 사장님 (음식점 등록, 수정 등)
+    ADMIN       // 관리자 (전체 시스템 관리)
+}
